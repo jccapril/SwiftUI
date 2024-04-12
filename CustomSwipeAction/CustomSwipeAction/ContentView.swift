97891453b949a819a95ca4c3e16b32dd78cfa589
Jason <jiangchencheng0409@gmail.com>
2024-04-12 15:53:02 +0800

    Custom Swipe Action

---
+++ CustomSwipeAction/CustomSwipeAction/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomSwipeAction
+//
+//  Created by Jason on 2024/4/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomSwipeAction/CustomSwipeAction/CustomSwipeActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwipeActionApp.swift
+//  CustomSwipeAction
+//
+//  Created by Jason on 2024/4/12.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwipeActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

8a0ff47dd697ebe8483853b94b524ef0416210d7
Jason <jiangchencheng0409@gmail.com>
2024-04-15 16:55:06 +0800

    搜索

--- Expense Tracker/Expense Tracker/View/TabViews/Search.swift
+++ Expense Tracker/Expense Tracker/View/TabViews/Search.swift
@@ -6,13 +6,41 @@
 //
 
 import SwiftUI
+import Combine
 
 struct Search: View {
+    /// View's Properties
+    @State private var searchText: String = ""
+    @State private var filterText: String = ""
+    let searchPublisher = PassthroughSubject<String, Never>()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView(.vertical) {
+                LazyVStack(spacing: 12) {
+                    
+                }
+            }
+            .overlay {
+                ContentUnavailableView("Search Transaction", systemImage: "magnifyingglass")
+                    .opacity(filterText.isEmpty ? 1 : 0)
+            }
+            .onChange(of: searchText, { oldValue, newValue in
+                if newValue.isEmpty {
+                    filterText = ""
+                }
+                searchPublisher.send(newValue )
+            })
+            .onReceive(searchPublisher.debounce(for: .seconds(0.3), scheduler: DispatchQueue.main), perform: { text in
+                filterText = text
+                print(text)
+            })
+            .searchable(text: $searchText)
+            .navigationTitle("Search")
+            .background(.gray.opacity(0.15))
+        }
     }
 }
 
 #Preview {
-    Search()
+    ContentView()
 }
--- Expense Tracker/Expense Tracker/View/TabViews/Settings.swift
+++ Expense Tracker/Expense Tracker/View/TabViews/Settings.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct Settings: View {
+    /// User Properties
+    @AppStorage("userName") private var userName: String = ""
+    /// App Lock Properties
+    @AppStorage("isAppLockEnbale") private var isAppLockEnbale: Bool = false
+    @AppStorage("lockWhenAppGoesBackground") private var lockWhenAppGoesBackground: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section("User Name"){
+                    TextField("iJustine", text: $userName)
+                }
+                
+                Section("App Lock"){
+                    Toggle("Enable App Lock", isOn: $isAppLockEnbale)
+                    
+                    if isAppLockEnbale {
+                        Toggle("Lock When App Goes Background", isOn: $lockWhenAppGoesBackground)
+                    }
+                }
+            }
+            .navigationTitle("Settings")
+        }
+        
     }
 }
 
 #Preview {
-    Settings()
+   ContentView()
 }

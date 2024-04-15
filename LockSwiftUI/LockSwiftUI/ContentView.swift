fa444291179e7f815eba585ac19930309ee30be9
Jason <jiangchencheng0409@gmail.com>
2024-04-15 18:15:23 +0800

    add LockSwiftUI

---
+++ LockSwiftUI/LockSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LockSwiftUI
+//
+//  Created by Jason on 2024/4/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LockSwiftUI/LockSwiftUI/LockSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LockSwiftUIApp.swift
+//  LockSwiftUI
+//
+//  Created by Jason on 2024/4/15.
+//
+
+import SwiftUI
+
+@main
+struct LockSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
